74e692be5ea6eb074de9dd5ce01cb02aebaeb9c3
phamngocdanhhcm <phamngocdanhhcm@gmail.com>
2022-07-12 22:43:30 +0700

    update

---
+++ sneaker-shop/src/components/Login.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class login extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    componentWillMount() {
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillReceiveProps(nextProps) {
+
+    }
+
+   
+
+    componentWillUpdate(nextProps, nextState) {
+
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default login;
